54d88809775f8458ee236d171b8f8f0adfbad7fc
Jonel Anderson <jonel.sacramento@unas.edu.pe>
2021-04-29 18:41:50 -0500

    primer log

---
+++ resources/views/principal.blade.php
@@ -0,0 +1,354 @@
+<!DOCTYPE html>
+<html lang="en">
+        <meta charset="utf-8">
+            <meta content="width=device-width, initial-scale=1.0" name="viewport">
+            <meta content="ie=edge" http-equiv="X-UA-Compatible">
+            <title>WebSite Dioner</title>
+            <link href="img/logo1.jpg" rel="icon" type="icon">
+            <link href="css/estilos.css" rel="stylesheet">
+            <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,600|Open+Sans" rel="stylesheet">
+            <link href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" rel="stylesheet">
+            <link href="estiloslogin.css" rel="stylesheet">
+            <link crossorigin="anonymous" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" rel="stylesheet">
+        </meta>
+    </head>
+    <body class="hidden">
+        <div class="centrado" id="onload">
+            <div class="lds-facebook">
+                <div>
+                </div>
+                <div>
+                </div>
+                <div>
+                </div>
+            </div>
+        </div>
+        <header>
+            <nav class="nav1" id="nav">
+                <div class="contenedor-nav">
+                    <div class="logo">
+                        <img alt="" src="img/Imagen.png">
+                        </img>
+                    </div>
+                    <div class="enlaces" id="enlaces">
+                        <a class="btn-header" href="#" id="enlace-inicio">
+                            Inicio
+                        </a>
+                        <a class="btn-header" href="#" id="enlace-equipo">
+                            Equipo
+                        </a>
+                        <a class="btn-header" href="#" id="enlace-servicio">
+                            Servicios
+                        </a>
+                        <a class="btn-header" href="#" id="enlace-trabajo">
+                            Trabajos
+                        </a>
+                        <a class="btn-header" href="#" id="enlace-contacto">
+                            Contacto
+                        </a>
+                    </div>
+                    <div class="icono" id="open">
+                        <span>
+                            ☰
+                        </span>
+                    </div>
+                </div>
+            </nav>
+            <div class="textos">
+                <video autoplay="autoplay" loop="loop" class="video_background" height="100%" preload="auto" muted>
+                <source src="img/fondo.mp4" type="video/mp4" />
+                </video>
+                <div >
+                    <div>
+                        <img src="img/lamborgini.png" height="120" width="200">
+                    </div>
+                    <h1>
+                        Anders Design 
+                    </h1>
+                    <h2>Agencia de desarrollo web</h2>
+                </div>
+            </div>
+        </header>
+        <main>
+            <section class="team contenedor" id="equipo">
+                <h3>
+                    Nuestro equipo
+                </h3>
+                <p class="after">
+                    Conoce a la gente asombrosa y creativa
+                </p>
+                <div class="card">
+                    <div class="content-card">
+                        <div class="people">
+                            <img alt="" src="img/people1.jpg">
+                            </img>
+                        </div>
+                        <div class="texto-team">
+                            <h4>
+                                K
+                            </h4>
+                            <p>
+                                Lorem ipsum dolor sit.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="content-card">
+                        <div class="people">
+                            <img alt="" src="img/people2.jpg">
+                            </img>
+                        </div>
+                        <div class="texto-team">
+                            <h4>
+                                C
+                            </h4>
+                            <p>
+                                Lorem ipsum dolor sit.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="content-card">
+                        <div class="people">
+                            <img alt="" src="img/people3.jpg">
+                            </img>
+                        </div>
+                        <div class="texto-team">
+                            <h4>
+                                M
+                            </h4>
+                            <p>
+                                Lorem ipsum dolor sit.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="about" id="servicio">
+                <div class="contenedor">
+                    <h3>
+                        Nuestros servicios
+                    </h3>
+                    <p class="after">
+                        Siempre mejorando para ti
+                    </p>
+                    <div class="servicios">
+                        <div class="caja-servicios">
+                            <figure>
+                                <img alt="" src="img/heart.png">
+                                </img>
+                            </figure>
+                            <h4>
+                                Creativos y asombrosos
+                            </h4>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur.
+                            </p>
+                            
+                        </div>
+                        <div class="caja-servicios">
+                            <figure>
+                                <img alt="" src="img/responsive.png">
+                                </img>
+                            </figure>
+                            <h4>
+                                Creativos y asombrosos
+                            </h4>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur.
+                            </p>
+                        </div>
+                        <div class="caja-servicios">
+                            <figure>
+                                <img alt="" src="img/efectos.png">
+                                </img>
+                            </figure>
+                            <h4>
+                                Creativos y asombrosos
+                            </h4>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur.
+                            </p>
+                        </div> 
+                    </div>
+                </div>
+            </section>
+            <section class="work contenedor" id="trabajo">
+                <h3>
+                    Nuestro trabajo
+                </h3>
+                <p class="after">
+                    Hacemos de algo simple algo extraordinario
+                </p>
+                <div class="botones-work">
+                    <ul>
+                        <li class="filter active" data-nombre="todos">
+                            Todos
+                        </li>
+                        <li class="filter" data-nombre="diseño">
+                            Diseño
+                        </li>
+                        <li class="filter" data-nombre="programacion">
+                            Programacion
+                        </li>
+                        <li class="filter" data-nombre="marketing">
+                            Marketing
+                        </li>
+                    </ul>
+                </div>
+                <div class="galeria-work">
+                    <div class="cont-work programacion">
+                        <div class="img-work">
+                            <figure>
+                                <img alt="" src="img/programacion1.jpeg">
+                                </img>
+                            </figure>
+                        </div>
+                        <div class="textos-work">
+                            <h4>
+                                Programacion
+                            </h4>
+                        </div>
+                    </div>
+                    <div class="cont-work programacion">
+                        <div class="img-work">
+                            <figure>
+                                <img alt="" src="img/programacion2.jpeg">
+                                </img>
+                            </figure>
+                        </div>
+                        <div class="textos-work">
+                            <h4>
+                                Programacion
+                            </h4>
+                        </div>
+                    </div>
+                    <div class="cont-work programacion">
+                        <div class="img-work">
+                            <figure>
+                                <img alt="" src="img/programacion3.jpeg">
+                                </img>
+                            </figure>
+                        </div>
+                        <div class="textos-work">
+                            <h4>
+                                Programacion
+                            </h4>
+                        </div>
+                    </div>
+                    <div class="cont-work diseño">
+                        <div class="img-work">
+                            <figure>
+                                <img alt="" src="img/diseño1.jpeg">
+                                </img>
+                            </figure>
+                        </div>
+                        <div class="textos-work">
+                            <h4>
+                                Diseño
+                            </h4>
+                        </div>
+                    </div>
+                    <div class="cont-work diseño">
+                        <div class="img-work">
+                            <figure>
+                                <img alt="" src="img/diseño2.jpeg">
+                                </img>
+                            </figure>
+                        </div>
+                        <div class="textos-work">
+                            <h4>
+                                Diseño
+                            </h4>
+                        </div>
+                    </div>
+                    <div class="cont-work diseño">
+                        <div class="img-work">
+                            <figure>
+                                <img alt="" src="img/diseño3.jpeg">
+                                </img>
+                            </figure>
+                        </div>
+                        <div class="textos-work">
+                            <h4>
+                                Diseño
+                            </h4>
+                        </div>
+                    </div>
+                    <div class="cont-work marketing">
+                        <div class="img-work">
+                            <figure>
+                                <img alt="" src="img/marketing1.jpeg">
+                                </img>
+                            </figure>
+                        </div>
+                        <div class="textos-work">
+                            <h4>
+                                Marketing
+                            </h4>
+                        </div>
+                    </div>
+                    <div class="cont-work marketing">
+                        <div class="img-work">
+                            <figure>
+                                <img alt="" src="img/marketing2.jpeg">
+                                </img>
+                            </figure>
+                        </div>
+                        <div class="textos-work">
+                            <h4>
+                                Marketing
+                            </h4>
+                        </div>
+                    </div>
+                    <div class="cont-work marketing">
+                        <div class="img-work">
+                            <figure>
+                                <img alt="" src="img/marketing3.jpeg">
+                                </img>
+                            </figure>
+                        </div>
+                        <div class="textos-work">
+                            <h4>
+                                Marketing
+                            </h4>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
+        <footer id="contacto">
+            <div class="footer contenedor">
+                <div class="marca-logo">
+                    <img alt="" src="img/Imagen.png">
+                    </img>
+                </div>
+                <div class="iconos">
+                    <i class="fab fa-youtube">
+                    </i>
+                    <i class="fab fa-facebook-square">
+                    </i>
+                    <i class="fab fa-github">
+                    </i>
+                </div>
+                <p>
+                    La pasión e innovación es lo que nos distingue del resto
+                </p>
+                <br/>
+                <br/>
+                <center>
+                    <small id="Pie">
+                        Derechos reservados Copyright by Anderson © 2021
+                        <address>
+                            Telf. 062-560343
+                        </address>
+                    </small>
+                </center>
+            </div>
+        </footer>
+        <script src="js/jquery.js">
+        </script>
+        <script src="js/main.js">
+        </script>
+        <script src="js/filtro.js">
+        </script>
+    </body>
+</html>
